fd51984066557c5ca049114161a2bf55a77964df
a-karaseva94 <a-karaseva94@mail.ru>
2024-06-30 14:33:22 +0300

    Add files via upload

---
+++ module 3.1.py
@@ -0,0 +1,24 @@
+calls = 0
+def count_calls():
+   global calls
+   calls += 1
+
+def string_info(string):
+   count_calls()
+   tuple_ = len(string), string.upper(), string.lower()
+   return tuple_
+
+def is_contains(string, list_to_search):
+   count_calls()
+   for i in list_to_search:
+      if string.lower() in i.lower():
+         return True
+   else:
+      return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
